2d11cf96cdc2e98d8ef6eb55a65ce4b7d789d67f
s92069870 <90398035+s92069870@users.noreply.github.com>
2022-05-21 08:44:55 +0530

    Add files via upload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,131 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+     <h3><u>What is the cripto currency</u></h3>
+    <p>A cryptocurrency, crypto-currency, crypto, or coin is a digital currency designed to work as a medium of exchange through a computer network that is not reliant on any central authority, 
+      such as a government or bank, to uphold or maintain it.
+
+Individual coin ownership records are stored in a digital ledger, which is a computerized database using strong cryptography to secure transaction records, to control the creation of additional 
+coins, and to verify the transfer of coin ownership. Despite their name, cryptocurrencies are not considered to be currencies in the traditional sense and while varying treatments have been applied to them, 
+including classification as commodities, securities, as well as currencies, cryptocurrencies are generally viewed as a distinct asset class in practice.Some crypto schemes use validators to maintain the cryptocurrency. 
+In a proof-of-stake model, owners put up their tokens as collateral. In return, they get authority over the token in proportion to the amount they stake. Generally, these token stakers get additional ownership in the
+ token over time via network fees, newly minted tokens or other such reward mechanisms.</p>
+ <img src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/06/how-to-buy-bitcoin.jpeg">
+
+<p>Cryptocurrency does not exist in physical form (like paper money) and is typically not issued by a central authority.
+ Cryptocurrencies typically use decentralized control as opposed to a central bank digital currency (CBDC). When a cryptocurrency is minted or created prior to issuance or issued by a single issuer, 
+ it is generally considered centralized. When implemented with decentralized control, each cryptocurrency works through distributed ledger technology, typically a blockchain, that serves as a public financial transaction database.
+ Traditional asset classes like currencies, commodities, and stocks, as well as macroeconomic factors, have modest exposures to cryptocurrency returns.</p>
+
+<p>A cryptocurrency is a tradable digital asset or digital form of money, built on blockchain technology that only exists online. 
+Cryptocurrencies use encryption to authenticate and protect transactions, hence their name. There are currently over a thousand different cryptocurrencies in the world.
+
+Over the last few years, cryptocurrency prices have risen and then fallen. Crypto marketplaces do not guarantee that an investor is completing a purchase or trade at the optimal price. As a result, 
+many investors take advantage of this by using arbitrage to find the difference in price across several markets.
+
+The first decentralized cryptocurrency was Bitcoin, which first released as open-source software in 2009. Since the release of Bitcoin, many other cryptocurrencies have been created.</p>
+
+<h3><u>What Can We Do With Bitcoin</u></h3>
+<p>
+  Bitcoin can be used for online transactions between individuals and nowadays, there are a lot of merchants that accept Bitcoin as a form of 
+  payment unlike before. They range from online retailers like Overstock and Newegg to local shops, bars and restaurants. Bitcoins can also be used to pay for hotels, flights, jewellery, apps and computer parts, or even a college degree.
+  <center><iframe width="560" height="315" src="https://www.youtube.com/embed/YsyueRWoa-M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+</p>
+<h3><u> History Of Cryptocurrency</u></h3>
+<p>
+  In 1983, the American cryptographer David Chaum conceived an anonymous cryptographic electronic money called ecash. Later, in 1995, he implemented it through Digicash, an early form of cryptographic electronic payments. 
+  Digicash required user software in order to withdraw notes from a bank and designate specific encrypted keys before it can be sent to a recipient. This allowed the digital currency to be untraceable by the issuing bank, the government, or any third party.</p>
+
+<p>In 1996, the National Security Agency published a paper entitled How to Make a Mint: the Cryptography of Anonymous Electronic Cash, describing a Cryptocurrency system, first publishing it in an MIT mailing listand later in 1997, in The American Law Review (Vol. 46, Issue 4).<br>
+
+
+<br>In 1998, Wei Dai published a description of "b-money", characterized as an anonymous, distributed electronic cash system. Shortly thereafter, Nick Szabo described bit gold. Like Bitcoin and other cryptocurrencies that would follow it, 
+bit gold (not to be confused with the later gold-based exchange, BitGold) was described as an electronic currency system which required users to complete a proof of work function with solutions being cryptographically put together and published.<br>
+
+<br>In 2009, the first decentralized cryptocurrency, Bitcoin, was created by presumably pseudonymous developer Satoshi Nakamoto. It used SHA-256, a cryptographic hash function, in its proof-of-work scheme. In April 2011, Namecoin was created as
+ an attempt at forming a decentralized DNS, which would make internet censorship very difficult. Soon after, in October 2011, Litecoin was released which used scrypt as its hash function instead of SHA-256. Another notable cryptocurrency,
+  Peercoin, used a proof-of-work/proof-of-stake hybrid.<br>
+
+<br>On 6 August 2014, the UK announced its Treasury had commissioned a study of cryptocurrencies, and what role, if any, they could play in the UK economy. The study was also to report on whether regulation should be considered.
+ Its final report was published in 2018, and it issued a consultation on cryptoassets and stablecoins in January 2021.<br>
+
+<br>In June 2021, El Salvador became the first country to accept Bitcoin as legal tender, after the Legislative Assembly had voted 62–22 to pass a bill submitted by President Nayib Bukele classifying the cryptocurrency as such.<br>
+
+<br>In August 2021, Cuba followed with Resolution 215 to recognize and regulate cryptocurrencies such as Bitcoin.<br>
+
+<br>In September 2021, the government of China, the single largest market for cryptocurrency, declared all cryptocurrency transactions illegal, 
+completing a crackdown on cryptocurrency that had previously banned the operation of intermediaries and miners within China.
+</p>
+
+ <h3>Mining</h3>
+        <center><iframe width="560" height="315" src="https://www.youtube.com/embed/672JlTIQ4RU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+        <p>In cryptocurrency networks, mining is a validation of transactions. For this effort, successful miners 
+            obtain new cryptocurrency as a reward. The reward decreases transaction fees by creating a complementary
+             incentive to contribute to the processing power of the network. The rate of generating hashes, which validate any 
+             transaction, has been increased by the use of specialized machines such as FPGAs and ASICs running complex hashing
+              algorithms like SHA-256 and scrypt. This arms race for cheaper-yet-efficient machines has existed since Bitcoin was 
+              introduced in 2009.[57]</p>
+
+          <p> With more people venturing into the world of virtual currency, generating hashes for validation has become more 
+              complex over time, forcing miners to invest increasingly large sums of money to improve computing performance. 
+              Consequently, the reward for finding a hash has diminished and often does not justify the investment in equipment 
+              and cooling facilities (to mitigate the heat the equipment produces), and the electricity required to run them. 
+              Popular regions for mining include those with inexpensive electricity, a cold climate, and jurisdictions with clear and
+               conducive regulations. By July 2019, Bitcoin's electricity consumption was estimated to be approximately 7 gigawatts, 
+               around 0.2% of the global total, or equivalent to the energy consumed nationally by Switzerland.</p>
+            
+         <p>   Some miners pool resources, sharing their processing power over a network to split the reward equally, 
+             according to the amount of work they contributed to the probability of finding a block. A "share" is awarded to members of
+              the mining pool who present a valid partial proof-of-work.</p>
+            
+          <p>  As of February 2018, the Chinese Government has halted trading of virtual currency, banned initial coin 
+              offerings and shut down mining. Many Chinese miners have since relocated to Canada and Texas.[61] One company 
+              is operating data centers for mining operations at Canadian oil and gas field sites, due to low gas prices. In June 2018
+              , Hydro Quebec proposed to the provincial government to allocate 500 Megawatts of power to crypto companies for mining.
+              According to a February 2018 report from Fortune, Iceland has become a haven for cryptocurrency miners in part because of its cheap
+               electricity.</p>
+            
+          <p>  In March 2018, the city of Plattsburgh in upstate New York put an 18-month moratorium on all cryptocurrency mining in an effort to
+               preserve natural resources and the "character and direction"
+               of the city. As of February 2022, Kazakhstan became the second-biggest crypto-currency mining country, producing 18.1% of the
+                global hash rate. The country has
+                built a compound containing 50,000 computers near Ekibastuz.</p>
+
+        <h3>GPU Price Rise</h3>   
+        <center><img src="https://imgsrv2.voi.id/xxEzBVXwVD-ueCJDnLuzyquNF0OJ5DXyHoUVKdRrtZM/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xMDMzMzgvMjAyMTExMTExODAwLW1haW4ucG5n.jpg" style="width: 560px; height:360px;"/></center>
+        <p>
+            An increase in cryptocurrency mining increased the demand for graphics cards (GPU) in 2017. 
+            (The computing power of GPUs makes them well-suited to generating hashes.) Popular favorites of cryptocurrency miners 
+            such as Nvidia's GTX 1060 and GTX 1070 graphics cards, as well as AMD's RX 570 and RX 580 GPUs, doubled or tripled in
+             price – or were out of stock. A GTX 1070 Ti which was released at a price of $450 sold for as much as $1100. 
+             Another popular card, the GTX 1060 (6 GB model) was released at an MSRP of $250, and sold for almost $500. RX 570
+              and RX 580 cards from AMD were out of stock for almost a year. Miners regularly buy up the entire stock of new GPU's as
+               soon as they are available.</p>
+
+        <p>
+              Nvidia has asked retailers to do what they can when it comes to selling GPUs to gamers instead of miners.
+               "Gamers come first for Nvidia," said Boris Böhles, PR manager for Nvidia in the German region.
+        </p>
+        <h3>
+            What is Blockchain and how does it work ? 
+        </h3>
+        <p>In the instance of cryptocurrencies this is where Blockchain comes in. 
+            Blockchain is a public ledger of all transactions that have ever happened within a network, 
+            available to everyone. This means that everyone in the network can see every account’s balance, allowing the 
+            digital currency to operate independently of a central bank. In order for a cryptocurrency network to work,
+             all participants must be in agreement about the legitimacy of balances and transactions.</p>
+        <h3>
+            Blockchain in healthcare
+        </h3>   
+        <p>There are many ways blockchain technology can improve the design and deployment of 
+            European Electronic Health Records. For example, blockchain can improve the security of patients records,
+             store consent – reducing administrative burden- and optimise the nursing profession.
+             <a href="https://www.openaccessgovernment.org/blockchain-based-technologies-for-sustainable-eu-healthcare-ecosystems/77617/">Read More</a>
+        </p> <br><br><br><br><br><br><br>
+
+ <div class="footer">
+           <p>Created by <a href="https://olq0czzynhy6uvncc3ckyw.on.drv.tw/www.Thanushalk.com/MY%20CV.html">Thanusha Deemantha</a></p>
+ </div>
   </div>
 </template>
 
@@ -41,18 +140,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+  p{
+  text-align: left;
+  text-transform:capitalize;
+  font-size: small;
+  padding-left: 10%;
+  color: whitesmoke;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h1{
+  color: whitesmoke;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+h3{
+  color: whitesmoke;
 }
-a {
-  color: #42b983;
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color:yellow;
+  color: rgb(221, 9, 9);
+  text-align: center;
+              
 }
 </style>
